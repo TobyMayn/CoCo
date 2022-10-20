8c637b55ac3b36cbb833b4109d9f190eb73f21f7
Tobias Mayn <tobiasmayn@gmail.com>
2022-10-20 13:57:20 +0200

    Updates

--- Environment.java
+++ Environment.java
@@ -2,13 +2,13 @@ import java.util.HashMap;
 import java.util.Map.Entry;
 
 class Environment {
-    private HashMap<String,Boolean> variableValues = new HashMap<String,Boolean>();
+    private HashMap<Variable,Boolean> variableValues = new HashMap<Variable,Boolean>();
     public Environment() { }	
-    public void setVariable(String name, Boolean value) {
+    public void setVariable(Variable name, boolean value) {
 	variableValues.put(name, value);
     }
     
-    public Boolean getVariable(String name){
+    public boolean getVariable(Variable name){
 	Boolean value = variableValues.get(name); 
 	if (value == null) { System.err.println("Variable not defined: "+name); System.exit(-1); }
 	return value;
@@ -16,7 +16,7 @@ class Environment {
     
     public String toString() {
 	String table = "";
-	for (Entry<String,Boolean> entry : variableValues.entrySet()) {
+	for (Entry<Variable,Boolean> entry : variableValues.entrySet()) {
 	    table += entry.getKey() + "\t-> " + entry.getValue() + "\n";
 	}
 	return table;
